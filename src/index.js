8323676ad3cd4c334a8224c096795258017e6e11
王宗垚 <wangzongyao@wangzongyaodeMacBook-Pro.local>
2019-06-02 22:18:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './webpack.png';
 
 function component() {
-  	var element = document.createElement('div');
+	var element = document.createElement('div');
 
-  	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
-  	return element;
+	// 将图像添加到我们现有的 div。
+	var myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
+	
+	return element;
 }
 
 document.body.appendChild(component());
