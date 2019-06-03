10085d37d473314cf96d8b6ea143278bdedaa08d
WangZongYao <wangzongyao@sunlands.com>
2019-06-03 20:45:49 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import { cube } from './math.js';
 import printMe from './print';
 import './style.css';
-import Icon from './webpack.png';
+// import Icon from './webpack.png';
 
 function component() {
 	var element = document.createElement('div');
-	var btn = document.createElement('button');
 
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
-
-	/* 	// 将图像添加到我们现有的 div。
-		var myIcon = new Image();
-		myIcon.src = Icon;
-		element.appendChild(myIcon); */
-
-	btn.innerHTML = 'Click me and check the console!';
-	btn.onclick = printMe;
-
-	element.appendChild(btn);
+	element.innerHTML = '5 cubed is equal to ' + cube(5);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
 		new webpack.NamedModulesPlugin(),
 		new webpack.HotModuleReplacementPlugin()
 	],
+	mode: "production",
 	module: {
 		rules: [
 			{
