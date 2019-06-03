6787ca0b49ca8a13982e467cb8ece05487a7906a
WangZongYao <wangzongyao@sunlands.com>
2019-06-03 15:56:16 +0800

    formate

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,12 @@ module.exports = {
 				use: [
 					'file-loader'
 				]
-			},{
-				        test: /\.(woff|woff2|eot|ttf|otf)$/,
-				         use: [
-				           'file-loader'
-				         ]
-				      }
+			}, {
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			}
 		]
 	}
 };
