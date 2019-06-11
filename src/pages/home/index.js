acc5465c89f5ea0bcd0b9320f03587412ddc6981
WangZongYao <wangzongyao@sunlands.com>
2019-06-11 20:45:08 +0800

    添加fetch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Button } from 'antd'
 
+import fetchJson from '../../commons/fetch/index.js'
 import styles from './index.less'
 
 class Home extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            name: '',
+        }
+    }
+
+    componentDidMount() {
+        const data = fetchJson('http://localhost:8080/user/login.json')
+        data.then((response) => {
+            //返回 object 类型
+            this.setState({ name: response.data.userName })
+        });
     }
 
     render() {
         return (
             <div className={styles['home-wrap']}>
-                <Button>Hello World</Button>
-        </div>
+                <Button>Hello {this.state.name}</Button>
+            </div>
         );
     }
 }
